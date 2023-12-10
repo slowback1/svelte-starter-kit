203f3eb672b41155ee6424ce6450cb2a6977a269
Andrew Wobeck <dwobeck@gmail.com>
2023-12-10 11:42:15 -0600

    Add test setup and message bus

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['./vitest-setup.ts'],
+		alias: [
+			{
+				find: /^svelte$/,
+				replacement: 'svelte/internal'
+			}
+		]
 	}
 });
