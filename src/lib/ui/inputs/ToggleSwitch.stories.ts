d2e720dd88d20d114ceb76c7c0540e73c1fd1328
Andrew Wobeck <dwobeck@gmail.com>
2023-12-16 17:37:19 -0600

    Add a ToggleSwitch component

---
+++ src/lib/ui/inputs/ToggleSwitch.stories.ts
@@ -0,0 +1,12 @@
+import ToggleSwitch from "$lib/ui/inputs/ToggleSwitch.svelte";
+
+export default {
+    title: "UI/ToggleSwitch",
+    component: ToggleSwitch,
+}
+
+export const Default = {
+    args: {
+        label: "Label"
+    }
+}
