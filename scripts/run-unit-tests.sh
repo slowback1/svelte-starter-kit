520ddef9e988b1a77f787f0f8b58c32b1e928d9e
Andrew Wobeck <dwobeck@gmail.com>
2023-12-27 16:15:59 -0600

    Finish off build and test CI scripts

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,6 +1,9 @@
 cd $(git rev-parse --show-toplevel) || exit 1
 
 TAG=frontend-tests-runner:latest
+NAME=frontend-test-runner
+
+docker container remove ${NAME} || true
 docker image remove ${TAG} || true
 
 docker build -t=${TAG} -f docker/Dockerfile --target=test .
@@ -8,4 +11,6 @@ docker build -t=${TAG} -f docker/Dockerfile --target=test .
 rm -rf ./testOutput || true
 mkdir testOutput
 
-docker run -v ./testOutput:/app/testOutput ${TAG}
+docker run --name=${NAME} -v ./testOutput:/app/testOutput ${TAG}
+
+docker wait ${NAME}
