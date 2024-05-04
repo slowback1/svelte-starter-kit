a170eec962cc2fdc6f5c246ecd1b72e2d7e043fc
Andrew Wobeck <dwobeck@gmail.com>
2024-05-04 16:51:42 -0500

    Refactor button styles to be more in line with current project standards

--- src/lib/ui/buttons/Button/Button.stories.ts
+++ src/lib/ui/buttons/Button/Button.stories.ts
@@ -1,52 +1,57 @@
 import Button from './Button.svelte';
-import type {StoryObj, Meta} from "@storybook/svelte";
+import type { StoryObj, Meta } from '@storybook/svelte';
 
 const meta: Meta = {
-    title: "UI/Button",
-    component: Button,
-    argTypes: {
-        variant: {
-            control: {type: "select"},
-            options: ["primary", "secondary", "text"]
-        },
-        size: {
-            control: {type: "select" },
-            options: ["small", "medium", "large"],
-        },
-    },
-}
+	title: 'UI/Button',
+	component: Button,
+	argTypes: {
+		variant: {
+			control: { type: 'select' },
+			options: ['primary', 'secondary', 'text']
+		},
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	}
+};
 
 export default meta;
 
-export const Default = {
-}
+export const Default = {};
 
 export const Secondary = {
-    args: {
-        variant: "secondary"
-    }
-}
+	args: {
+		variant: 'secondary'
+	}
+};
 
 export const Text = {
-    args: {
-        variant: "text"
-    }
-}
+	args: {
+		variant: 'text'
+	}
+};
 
 export const Small = {
-    args: {
-        size: "small"
-    }
-}
+	args: {
+		size: 'small'
+	}
+};
 
 export const Medium = {
-    args: {
-        size: "medium"
-    }
-}
+	args: {
+		size: 'medium'
+	}
+};
 
 export const Large = {
-    args : {
-        size: "large"
-    }
-}
+	args: {
+		size: 'large'
+	}
+};
+
+export const Disabled = {
+	args: {
+		disabled: true
+	}
+};
--- src/lib/ui/buttons/Button/Button.svelte
+++ src/lib/ui/buttons/Button/Button.svelte
@@ -1,101 +1,102 @@
 <script lang="ts">
-  export let testId: string = "";
-  type ButtonVariant = "primary" | "secondary" | "text";
-  type ButtonSize = "small" | "medium" | "large";
-  export let variant: ButtonVariant = "primary";
-  export let size: ButtonSize = "medium";
-  export let href: string = undefined;
-  export let disabled: boolean = false;
-  export let tabIndex: number = 0;
-  export let onClick: (event: Event) => void = () => {
-  };
+	import type { ButtonSize, ButtonVariant } from '$lib/ui/buttons/Button/ButtonTypes';
 
-  const isSecondary = variant === "secondary";
-  const isPrimary = variant === "primary";
-  const isText = variant === "text";
+	export let testId: string = '';
+	export let variant: ButtonVariant = 'primary';
+	export let size: ButtonSize = 'medium';
+	export let href: string = undefined;
+	export let disabled: boolean = false;
+	export let tabIndex: number = 0;
+	export let onClick: (event: Event) => void = () => {};
 
-  const isSmall = size === "small";
-  const isLarge = size === "large";
+	const isSecondary = variant === 'secondary';
+	const isPrimary = variant === 'primary';
+	const isText = variant === 'text';
 
-  const tag = !!href ? "a" : "button";
-  const role = tag === "a" ? "link": "button";
+	const isSmall = size === 'small';
+	const isLarge = size === 'large';
+
+	const tag = !!href ? 'a' : 'button';
+	const role = tag === 'a' ? 'link' : 'button';
 </script>
 
-<svelte:element this={tag}
-                class="button"
-                class:button-large={isLarge}
-                class:button-small={isSmall}
-                class:button-primary={isPrimary}
-                class:button-secondary={isSecondary}
-                class:button-text={isText}
-                {href}
-                on:click={onClick}
-                disabled="{disabled}"
-                data-testid={testId}
-                role={role}
-                tabIndex="{tabIndex}"
-                >
-    <slot> Button Content Goes Here </slot>
+<svelte:element
+	this={tag}
+	class="button-base button"
+	class:button-large={isLarge}
+	class:button-small={isSmall}
+	class:button-primary={isPrimary}
+	class:button-secondary={isSecondary}
+	class:button-text={isText}
+	{href}
+	on:click={onClick}
+	{disabled}
+	data-testid={testId}
+	{role}
+	{tabIndex}
+>
+	<slot>Button Content Goes Here</slot>
 </svelte:element>
 
 <style>
-    .button {
-        border-radius: 4px;
-        border: 1px solid;
-        padding: 8px 16px;
-        text-decoration: none;
-        font-family: var(--font-family-primary);
-        font-size: var(--font-size-medium);
-        transition: background-color 0.5s ease-in-out;
-    }
+	.button-base {
+		--button-border-radius: 8px;
+		--button-border-width: 1px;
+		--button-padding-x: 0.5rem;
+		--button-padding-y: 1rem;
+		--button-font-size: var(--font-size-medium);
+		--button-color: var(--color-background);
+		--button-border-color: var(--color-font);
+		--button-disabled-opacity: 0.5;
+	}
 
-    .button:hover {
-        cursor: pointer;
-    }
+	.button-small {
+		--button-font-size: var(--font-size-small);
+	}
 
-    .button:disabled {
-      opacity: 0.5;
-    }
+	.button-large {
+		--button-font-size: var(--font-size-large);
+	}
 
-    .button-small {
-        padding: 4px 8px;
-        font-size: var(--font-size-small);
-    }
+	.button-primary {
+		--button-color: var(--color-base-blue);
+	}
 
-    .button-large {
-        padding: 12px 24px;
-    }
+	.button-secondary {
+		--button-color: var(--color-base-green);
+	}
 
-    .button-primary {
-        background-color: var(--color-primary-background);
-        color: var(--color-primary-font);
-        border-color: var(--color-primary-border);
-    }
+	.button-text {
+		--button-color: var(--color-background);
+		--button-border-color: transparent;
+	}
 
-    .button-primary:hover, .button-primary:focus {
-        background-color: var(--color-primary-background-highlight);
-    }
+	.button-text:hover,
+	.button-text:focus {
+		--button-color: var(--color-base-blue);
+		--button-border-color: var(--color-font);
+	}
 
-    .button-secondary {
-        background-color: transparent;
-        color: var(--color-secondary-font);
-        border-color: var(--color-secondary-border);
-    }
+	.button {
+		border-radius: var(--button-border-radius);
+		border: var(--button-border-width) solid var(--button-border-color);
+		padding: var(--button-padding-x) var(--button-padding-y);
+		text-decoration: none;
+		font-family: var(--font-family-primary);
+		font-size: var(--button-font-size);
+		transition: background-color 0.5s ease-in-out;
+		background-color: color-mix(in lab, var(--button-color) 80%, var(--color-background));
+		color: color-mix(in lab, var(--button-color) 20%, var(--color-font));
+	}
 
-    .button-secondary:hover, .button-secondary:focus {
-        background-color: var(--color-secondary-background);
-        color: var(--color-secondary-background-font);
-    }
+	.button:hover,
+	.button:focus {
+		cursor: pointer;
+		background-color: color-mix(in lab, var(--button-color) 95%, var(--color-background));
+	}
 
-    .button-text {
-        background-color: transparent;
-        color: var(--color-font);
-        border-color: transparent;
-    }
-
-    .button-text:hover, .button-text:focus {
-        background-color: var(--color-primary-background);
-        color: var(--color-primary-font);
-        border-color: var(--color-primary-border);
-    }
+	.button:disabled {
+		cursor: not-allowed;
+		opacity: var(--button-disabled-opacity);
+	}
 </style>
