13ad91c71d8069e52772e3f888d8675c5893b30e
Andrew Wobeck <dwobeck@gmail.com>
2024-11-24 19:14:39 -0600

    Add a utility function around getting a random number

---
+++ src/lib/utils/numberUtils.spec.ts
@@ -0,0 +1,18 @@
+﻿import { getRandomNumber } from '$lib/utils/numberUtils';
+
+describe('numberUtils', () => {
+	describe('getRandomNumber', () => {
+		it.each([
+			[0, 1],
+			[1, 10],
+			[10, 100],
+			[100, 1000],
+			[0.5, 1.5],
+			[Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+		])('should return a number between %s and %s', (min, max) => {
+			const randomNumber = getRandomNumber(min, max);
+			expect(randomNumber).toBeGreaterThanOrEqual(min);
+			expect(randomNumber).toBeLessThanOrEqual(max);
+		});
+	});
+});
