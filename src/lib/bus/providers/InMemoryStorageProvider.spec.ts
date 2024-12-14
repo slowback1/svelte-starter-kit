4de989b12a43518fda8b3b3f1975e0277dfb8b8b
Andrew Wobeck <dwobeck@gmail.com>
2024-12-13 18:02:50 -0600

    Implement an "In Memory" storage provider for the message bus.

---
+++ src/lib/bus/providers/InMemoryStorageProvider.spec.ts
@@ -0,0 +1,31 @@
+﻿import InMemoryStorageProvider from '$lib/bus/providers/inMemoryStorageProvider';
+import { beforeEach } from 'vitest';
+
+describe('InMemoryStorageProvider', () => {
+	let storageProvider: InMemoryStorageProvider;
+
+	beforeEach(() => {
+		storageProvider = new InMemoryStorageProvider();
+	});
+
+	it('should set an item', () => {
+		storageProvider.setItem('key', 'value');
+		expect(storageProvider.getItem('key')).toBe('value');
+	});
+
+	it("should return undefined if the item doesn't exist", () => {
+		expect(storageProvider.getItem('key')).toBeUndefined();
+	});
+
+	it('should return the store', () => {
+		storageProvider.setItem('key', 'value');
+		expect(storageProvider.getStore()).toEqual({ key: 'value' });
+	});
+
+	it('can store multiple items', () => {
+		storageProvider.setItem('key1', 'value1');
+		storageProvider.setItem('key2', 'value2');
+
+		expect(storageProvider.getStore()).toEqual({ key1: 'value1', key2: 'value2' });
+	});
+});
---
+++ src/lib/bus/providers/inMemoryStorageProvider.ts
@@ -0,0 +1,17 @@
+﻿import type IStorageProvider from '$lib/bus/providers/IStorageProvider';
+
+export default class InMemoryStorageProvider implements IStorageProvider {
+	private store: Record<string, any> = {};
+
+	getItem(key: string) {
+		return this.store[key];
+	}
+
+	setItem(key: string, value: any) {
+		this.store[key] = value;
+	}
+
+	getStore() {
+		return this.store;
+	}
+}
