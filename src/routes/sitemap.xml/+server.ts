fb149b6513e1da442e6d5a6b78909f042c485f7a
Andrew Wobeck <dwobeck@gmail.com>
2024-12-13 19:09:21 -0600

    Implement a solution to automatically generate a site.xml  on build

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,71 @@
+﻿export const site = 'https://yourdomain.com';
+
+const pages: string[] = [];
+
+const sveltePageFiles = import.meta.glob('/src/routes/**/+page.svelte');
+const filePaths = Object.keys(sveltePageFiles);
+
+type PageOverride = {
+	/** This should a numeric value that is between 0.0 and 1.0 */
+	priority?: string;
+	/** This should be one of the following values: always, hourly, daily, weekly, monthly, yearly, never */
+	changeFreq?: string;
+};
+
+const overrides: Record<string, PageOverride> = {
+	'': {
+		priority: '1.0',
+		changeFreq: 'monthly'
+	}
+};
+
+for (const filePath of filePaths) {
+	const page = filePath.replace('/src/routes/', '').replace('+page.svelte', '');
+	pages.push(page);
+}
+
+function getOverride(page: string): PageOverride {
+	return overrides[page] ?? {};
+}
+
+function getPriority(page: string) {
+	return getOverride(page).priority ?? 0.5;
+}
+
+function getChangeFreq(page: string) {
+	return getOverride(page)?.changeFreq ?? 'daily';
+}
+
+export const prerender = true;
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+	const body = buildSitemapXml(pages);
+	const response = new Response(body);
+	response.headers.set('Cache-Control', 'max-age=0, s-maxage=3600');
+	response.headers.set('Content-Type', 'application/xml');
+
+	return response;
+}
+
+const buildSitemapXml = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  ${pages
+		.map(
+			(page) => `
+  <url>
+    <loc>${site}/${page}</loc>
+    <changefreq>${getChangeFreq(page)}</changefreq>
+    <priority>${getPriority(page)}</priority>
+  </url>
+  `
+		)
+		.join('')}
+</urlset>`;
---
+++ src/routes/sitemap.xml/server.spec.ts
@@ -0,0 +1,53 @@
+﻿import { GET as getSiteMap, site } from './+server';
+
+describe('Sitemap', () => {
+	it('should return a sitemap.xml', async () => {
+		const response = await getSiteMap();
+		expect(response.status).toBe(200);
+
+		const headers = response.headers;
+
+		expect(headers.get('Content-Type')).toBe('application/xml');
+	});
+
+	it('Should set the max-age headers to 3600', async () => {
+		const response = await getSiteMap();
+		const headers = response.headers;
+
+		expect(headers.get('Cache-Control')).toBe('max-age=0, s-maxage=3600');
+	});
+
+	it('response body is valid xml', async () => {
+		const response = await getSiteMap();
+		const body = await response.text();
+
+		expect(() => new DOMParser().parseFromString(body, 'text/xml')).not.toThrow();
+	});
+
+	async function getHomeRecord() {
+		const body = await getSiteMap().then((response) => response.text());
+
+		const xml = new DOMParser().parseFromString(body, 'text/xml');
+
+		const urls = xml.querySelectorAll('urlset > url');
+
+		const home = Array.from(urls).find(
+			(url) => url.querySelector('loc').textContent === `${site}/`
+		);
+
+		expect(home).not.toBeFalsy();
+		return home;
+	}
+
+	it('the home page should have a priority of 1.0', async () => {
+		const home = await getHomeRecord();
+
+		expect(home.querySelector('priority').textContent).toBe('1.0');
+	});
+
+	it('the home page should have a changefreq of monthly', async () => {
+		const home = await getHomeRecord();
+
+		expect(home.querySelector('changefreq').textContent).toBe('monthly');
+	});
+});
