4e16437a57a54190e4c81263a30683ed3e3e9941
Andrew Wobeck <dwobeck@gmail.com>
2025-02-23 17:36:56 -0600

    Add to the initial setup script to install the cypress dependencies

--- scripts/initial-setup.sh
+++ scripts/initial-setup.sh
@@ -1,5 +1,12 @@
+# Ensure we're in a valid git repo, and cd to the root
 cd $(git rev-parse --show-toplevel) || exit 1
 
+# Copy the example config file to the correct location
 cp ./static/config/config.example.json ./static/config/config.json
 
+# Install the node modules in the base application
+npm install
+
+# Install the node modules in the cypress tests
+cd ./cypress || exit 1
 npm install
